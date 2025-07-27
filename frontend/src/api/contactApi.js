b0ed5e0d9faa7c85560131615ba8afe32107964b
AbhishekPatil1310 <ap7497493@gmail.com>
2025-07-27 14:16:25 +0530

    Update contactApi.js

--- frontend/src/api/contactApi.js
+++ frontend/src/api/contactApi.js
@@ -1,6 +1,6 @@
 // src/api/contactApi.js
 import axios from 'axios';
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api/v1';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://patil-project-axrt.vercel.app';
 
 export const sendContactMessage = async (data) => {
   const res = await axios.post(`${BASE_URL}/contact`, data);
